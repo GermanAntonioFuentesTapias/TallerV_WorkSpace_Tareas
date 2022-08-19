5e586e8977338c76c756fabfd70fb38252118679
German Fuentes <gafuentest@unal.edu.co>
2022-08-19 18:32:42 -0500

    Nueva configuración
    Se paso de repositorio por unos errores de vinculación y se paso lo que se tenia hasta el momento.

---
+++ EdadHexBin/Src/main.c
@@ -0,0 +1,31 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 19, 2022
+ *      Author: German Antonio Fuentes Tapias
+ */
+/**
+ *  Se declara las variables necesarias para el funcionamiento de la tare
+ */
+unsigned char Age = 0; // La edad que se tiene al momento de diseñar el programa
+unsigned short Number_days_Year = 0; //Numero de dias  en un año sin contar años bisiesto
+unsigned char Number_hours_days = 0; //Numero de horas de un dia
+unsigned short Number_Seconds_hours = 0; //Numero de segundos que se tiene en una hora
+unsigned short Number_days_live = 0; // Numero de dias vividos desde la fecha de nacimiento hasta el 17/08/2022
+unsigned int Number_days_live_hours = 0; // Numero de horas vividas desde el dia de nacimiento hasta el 17/08/2022 a medio dia
+unsigned int Number_days_live_seconds = 0; // Numero de segundos vividos desde el dia de nacimiento hasta el 17/08/2022 a medio dia
+//Prueba
+/*Funcion principal*/
+
+int main(){
+
+	Age = 23;
+	Number_days_Year = 365;
+	Number_Seconds_hours = 3600;
+
+  while(1){
+  }
+  }
+
+
+
