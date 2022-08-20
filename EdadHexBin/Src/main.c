c7bc862376fa3978d841a6ecc5b548588b226865
German Fuentes <gafuentest@unal.edu.co>
2022-08-19 19:00:53 -0500

    Update main.c
    Aclaracion con comentarios

--- EdadHexBin/Src/main.c
+++ EdadHexBin/Src/main.c
@@ -3,25 +3,62 @@
  *
  *  Created on: Aug 19, 2022
  *      Author: German Antonio Fuentes Tapias
+ *      Estudiante de Ingeniería Física
+ *
+ *
+ *
+ *      <<<<<<<<<Nota= Recordar que cumplo el mismo dia de LuisFelipe>>>>>>>>>>
  */
 /**
- *  Se declara las variables necesarias para el funcionamiento de la tare
+ *  Se declara las variables necesarias para el funcionamiento de la tarea
+ *  Ademas se tomo las variables con variable inicial cero
  */
+/*Se creara una variable de 8 bits que sera suficiente para la edad*/
 unsigned char Age = 0; // La edad que se tiene al momento de diseñar el programa
-unsigned short Number_days_Year = 0; //Numero de dias  en un año sin contar años bisiesto
-unsigned char Number_hours_days = 0; //Numero de horas de un dia
-unsigned short Number_Seconds_hours = 0; //Numero de segundos que se tiene en una hora
-unsigned short Number_days_live = 0; // Numero de dias vividos desde la fecha de nacimiento hasta el 17/08/2022
-unsigned int Number_days_live_hours = 0; // Numero de horas vividas desde el dia de nacimiento hasta el 17/08/2022 a medio dia
-unsigned int Number_days_live_seconds = 0; // Numero de segundos vividos desde el dia de nacimiento hasta el 17/08/2022 a medio dia
-//Prueba
-/*Funcion principal*/
+
+/*Se creara una variable de 16 bits, ya que 8 no es suficiente para los dias del año */
+unsigned short N_days_Year = 0; //Numero de dias  en un año sin contar años bisiesto
+
+/*Se creara una variable de 8 bits que es suficiente para las horas del dia*/
+unsigned char N_hours_days = 0; //Numero de horas de un dia
+
+/*Se creara una variable de 16 bits, ya que 8 no es suficiente para los segundos de una hora */
+unsigned short N_Seconds_hours = 0; //Numero de segundos que se tiene en una hora
+
+/*Se creara una variable de 16 bits, ya que 8 no es suficiente para los dias que se ha vivido desde el nacimiento*/
+unsigned short N_days_live = 0; // Numero de dias vividos desde la fecha de nacimiento hasta el 17/08/2022
+
+/*Se creara una variable de 32 bits, ya que 8 ni 16 fueron suficientes para las horas de dias vividos */
+unsigned int N_days_live_hours = 0; // Numero de horas vividas desde el dia de nacimiento hasta el 17/08/2022 a medio dia
+
+/*Se creara una variable de 32 bits, ya que 8 no es suficiente para los dias del año */
+unsigned int N_days_live_seconds = 0; // Numero de segundos vividos desde el dia de nacimiento hasta el 17/08/2022 a medio dia
+
+/*Funcion principal del proyecto*/
 
 int main(){
+    /* Se definen las 3 variables necesarias durante todo el codigo, las cuales son Año, numero de dias y segundos de una hora*/
 
-	Age = 23;
-	Number_days_Year = 365;
-	Number_Seconds_hours = 3600;
+	Age = 23; // Mi edad completa hasta el 17/11/1998
+	N_days_Year = 365;// Los dias del año sin bisiesto
+	N_Seconds_hours = 3600;// Segundos que tiene una hora
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   while(1){
   }
