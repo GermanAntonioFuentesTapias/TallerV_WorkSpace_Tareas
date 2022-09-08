885aa231a724b19a00ea4cc622723a233c3bf801
German Fuentes <gafuentest@unal.edu.co>
2022-09-08 12:03:30 -0500

    Configuración Basica Timers
    Se configuro los timers completamente

--- BasicConfig/Src/main.c
+++ BasicConfig/Src/main.c
@@ -18,38 +18,77 @@
  */
 
 #include <stdint.h>
+#include "stm32f4xx.h"
+#include "GPIOxDriver.h"
+#include "BasicTimer.h"
+//uint16_t data = 0;
+//uint16_t *ptrEjemplo;
+//uint32_t ValorPuntero = 0;
 
-uint16_t data = 0;
-uint16_t *ptrEjemplo;
-uint32_t ValorPuntero = 0;
+BasicTimer_Handler_t  handlerTimer2 ={0};
+GPIO_Handler_t       handlerBlinkyLed = {0};
 
+uint8_t blinky = 0;
 int main(void){
 //    Iniciando variables
-	data= 57;
-
-	// Cambiando el valor de data en +32
-	data +=32;
-
-	// A que el puntero apunte
-
-	ptrEjemplo = &data;
-
-	ValorPuntero = (uint32_t) ptrEjemplo;
-
-    *ptrEjemplo +=2;
-
-    // Incrementamos posición de memoria
-
-    ptrEjemplo++;
-
-    *ptrEjemplo= 0xAC;
+	handlerBlinkyLed.pGPIOx= GPIOA;
+//
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinNumber =        PIN_5;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinMode   =        GPIO_MODE_OUT;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinOPType  =       GPIO_OTYPE_PUSHPULL;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinPuPdControl =   GPIO_PUPDR_NOTHING;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinSpeed =         GPIO_OSPEED_MEDIUM;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinAltFunMode =    AF0;
 
 
+	GPIO_Config(&handlerBlinkyLed);
+	 handlerTimer2.ptrTIMx = TIM2;
+	 handlerTimer2.TIMx_Config.TIMx_mode = BTIMER_MODE_UP; // CUENTA HACIA ARRIBA
+	 handlerTimer2.TIMx_Config.TIMx_speed = BTIMER_SPEED_100us; // La velocidad
+	 handlerTimer2.TIMx_Config.TIMx_period = 2500;
+	 handlerTimer2.TIMx_Config.TIMx_interruptEnable = 1;
+	 BasicTimer_Config(&handlerTimer2);
 
-
+//	data= 57;
+//
+//	// Cambiando el valor de data en +32
+//	data +=32;
+//
+//	// A que el puntero apunte
+//
+//	ptrEjemplo = &data;
+//
+//	ValorPuntero = (uint32_t) ptrEjemplo;
+//
+//    *ptrEjemplo +=2;
+//
+//    // Incrementamos posición de memoria
+//
+//    ptrEjemplo++;
+//
+//    *ptrEjemplo= 0xAC;
+//
+//
+//
+//
 	while(1){
-
-	}
+//
+   }
 
 	return 0;
+
+// Call Back del timer
 }
+void BasicTimerX_CallBack(void){
+
+	blinky = !blinky;
+
+	if(blinky){
+		GPIO_WritePin(&handlerBlinkyLed, SET);
+	}else{
+		GPIO_WritePin(&handlerBlinkyLed, RESET);
+	}
+}
+
+
+
